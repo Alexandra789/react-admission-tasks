213cb0b81255bdcd65059dac01dfa3dd99b5566e
Заковинько Александра Сергеевна <zakovinkoas@ad.atlant.by>
2025-03-05 11:18:57 +0300

    done first task

---
+++ task1/task1.js
@@ -0,0 +1,20 @@
+let terminalBody = document.querySelector('.terminal__body');
+
+function logMessage(firstLetter, lastLetter) {
+    const firstLetterCode = firstLetter.charCodeAt(0);
+    const lastLetterCode = lastLetter.charCodeAt(0) + 1;
+    let currentCode = firstLetterCode;
+
+    function logCharactersWithDelay() {
+        if (currentCode < lastLetterCode) {
+            console.log(String.fromCharCode(currentCode));
+            terminalBody.innerHTML += `<p>${String.fromCharCode(currentCode)}</p>`;
+            currentCode++;
+            setTimeout(() => logCharactersWithDelay(), 1000);
+        }
+
+    }
+    logCharactersWithDelay();
+}
+
+logMessage('A', 'F');
